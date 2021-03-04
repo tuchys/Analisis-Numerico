ad441777cdc0481ee2d5b43922d26e6b613d8726
JoseMarioAriasAcevedo <78364618+JoseMarioAriasAcevedo@users.noreply.github.com>
2021-03-04 08:53:50 -0500

    Add files via upload

---
+++ Parciales/Parcial 1/parcial.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 07:35:28 2021
+
+@author: josem
+"""
+
+
+import matplotlib.pyplot as plt
+
+n = 4
+n = n
+division = 0
+multiplicacion = 0
+adicion = 0
+total = 0
+
+suma =  (1/3)*((2*n) +1) * (n*(n + 1 )/2)
+
+for i in range(1, n):
+    
+    multiplicacion += 4
+    division += 2
+    adicion += 2
+
+
+    
+print("El resultado es: ", suma)
+print("numero de multiplicaciones hechas: ", multiplicacion)
+print("numero de divisiones hechas: ", division)
+print("numero de sumas hechas: ", adicion)
+total = multiplicacion+division+adicion
+print("El total de operaciones fue: ", total )
+
+
